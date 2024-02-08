ac79b97a090323ecc34c31aa7206fb193e432958
anthow <anthonyenglebert@gmail.com>
2024-02-08 13:09:59 +0100

    /

--- src/components/intervenants.js
+++ src/components/intervenants.js
@@ -74,11 +74,12 @@ const Intervants = ({ }) => (
             <section className=" flex flex-col text-white items-center bg-engage">
 
 
-                <img alt="Logo du parti politique 'Les Engagés', participant à la table ronde politique de l'Interfédération des CISP, le 14 mars 2024 à Namur" src="https://res.cloudinary.com/docshhbla/image/upload/v1705067783/les-engages-logo_qqno6i.jpg" className=""></img>
+                <img alt="Logo du parti politique 'Les Engagés', participant à la table ronde politique de l'Interfédération des CISP, le 14 mars 2024 à Namur" 
+                src="https://res.cloudinary.com/docshhbla/image/upload/v1707387649/2e94f2aa8e40479ca0ac1fd98a70aa7e_1_j9n2n4.jpg" className=""></img>
                 <div className="gap-10 my-5 flex flex-col w-10/12 m-auto gap-10 items-center">
                     <div className="flex flex-col gap-5">
-                        <h3 className=" text-xl font-bold">Les Engagés</h3>
-                        <h4 className="text-lg italic"> intervenant(e) encore à confirmer </h4>
+                        <h3 className=" text-xl font-bold">Benoit Dispa - Les Engagés</h3>
+                        <h4 className="text-lg italic"> Député wallon, député au Parlement de la fédération Wallonie-Bruxelles  </h4>
                     </div>
                     <p className="hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempor tempor urna eget vulputate. Maecenas consequat turpis sed nisi pretium, sed congue eros facilisis.
 
@@ -95,7 +96,7 @@ const Intervants = ({ }) => (
                 <div className="gap-10 my-5 flex flex-col w-10/12 m-auto gap-10 items-center">
                     <div className="flex flex-col gap-5">
                         <h3 className=" text-xl font-bold">Amaury Alexandre - Défi</h3>
-                        <h4 className="text-lg italic"> Député provincial à Namur.<br /> </h4>
+                        <h4 className="text-lg italic"> Député provincial à Namur<br /> </h4>
                     </div>
                     <p className="hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempor tempor urna eget vulputate. Maecenas consequat turpis sed nisi pretium, sed congue eros facilisis.
 
